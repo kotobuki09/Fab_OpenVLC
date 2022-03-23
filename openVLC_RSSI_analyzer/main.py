0553d36f4f48155b5ab89cef987efb87fec6edb1
Fabrizio Giuliano <fabrizio@unipa.it>
2022-03-23 15:11:26 +0100

    Add OpenVLC_RSSI_analyzer script Add raw examples of RSSI measurements Add pruedebug patch for RSSI extraction

---
+++ openVLC_RSSI_analyzer/main.py
@@ -0,0 +1,48 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+#Next experiments:
+#1. out of box: 0.5m
+#2. out of box: 1m
+#3. out of box: 1.5m
+#4. out of box: 2m
+#5. out of box: 2m + light interferer (ex smartphone lamp...)
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+filename_idle="raw/dump_idle.raw"
+filename_iperf="raw/dump_iperf_400K.raw"
+def get_rssi(filename):
+    df = pd.read_csv(filename,skiprows=23, sep=" ",names=["pos", "1", "2", "3","4","5"])
+    del df["pos"]
+    del df["5"]
+    rssi_array=df.to_numpy().flatten()
+    rssi=[int(rssi_array[i],16) for i in range(len(rssi_array))]
+    return rssi
+def main():
+
+    rssi_idle=get_rssi(filename_idle)
+    rssi_iperf=get_rssi(filename_iperf)
+    fig = plt.figure()
+    plt.plot(rssi_idle,label="IDLE")
+    plt.plot(rssi_iperf,label="IPERF")
+
+    plt.legend()
+    plt.grid()
+    plt.xlabel("#instruction event")
+    plt.ylabel("RSSI level")
+    plt.xlim(0,300)
+    fig.savefig("IDLE vs IPERF RSSI.pdf")
+    fig.savefig("IDLE vs IPERF RSSI.png")
+    plt.show()
+
+    print("wait...")
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
