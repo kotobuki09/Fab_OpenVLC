4f20a8068776c4dc3e6f84a23cce4d6912c28a84
kotobuki09 <keeningo@gmail.com>
2022-03-29 16:18:42 +0200

    Summary

--- ControlUnit/fabfile.py
+++ ControlUnit/fabfile.py
@@ -1,9 +1,14 @@
 import fabric.api as fab
 import fabric.utils
-from fabric.api import run, env
+from fabric.api import run, env, execute, roles
 from fabric.context_managers import cd
 import time
-from fabric.api import execute
+
+
+
+#env.hosts={'debian@10.8.10.5'}
+#env.roledefs = { 'vlc1': ['debian@10.8.10.5'], 'vlc2': ['debian@10.8.10.8'] }
+#env.passwords = { 'debian@10.8.10.5:22':'temppwd', 'debian@10.8.10.8:22':'temppwd' }
 
 #nice workaround solution to embed the host access parameters as a fabric task
 @fab.task
@@ -16,6 +21,10 @@ def vlc2():
     env.hosts={'debian@10.8.10.8'}
     env.passwords = {'debian@10.8.10.8:22':'temppwd'}
 
+@fab.task
+def getRSSI():
+    output=fab.sudo("python3 getRSSI.py")
+    return output
 
 @fab.task
 def start_iperf_client(type=""):
@@ -30,24 +39,29 @@ def start_iperf_client(type=""):
 
 @fab.task
 def stop_iperf():
-    fab.run("killall -9 iperf")
+    fab.run("killall iperf")
 
 @fab.task
 def start_iperf_server(type=""):
     #start iperf
-    #fab.run("killall -9 iperf")
+    #fab.sudo("killall -q iperf",pty=True)
     if type=="vlc":
-        command = "iperf -u -l 800 -s -i3 -B 192.168.0.2 -p 10001"
+        rx_host="192.168.0.2"
+        #command = "iperf -u -l 800 -s -i3 -B 192.168.0.2 -p 10001"
     elif type=="wifi":
-        command = "iperf -u -l 800 -s -i3 -B 192.168.12.26 -p 10002"
+        rx_host="192.168.12.26"
+        #command = "iperf -u -l 800 -s -i3 -B 192.168.12.26 -p 10002"
     else:
-        command = "iperf -u -l 800 -s -i3 -B 192.168.10.2 -p 10003"
+        rx_host="192.168.10.2"
+        command = "iperf -u -l 800 -s -i3 -B 192.168.10.2 -p 10002"
+
+    command = "iperf -u -l 800 -s -i3 -B {} -p 10003".format(rx_host)
     fab.run(command, pty=False)
 
 @fab.task
 def setup_wifi_ap():
     fab.sudo("service dnsmasq stop")
-    fab.sudo("create_ap -n wlan0 MyAccessPoint 12345678")
+    fab.sudo("create_ap -c 6 -n wlan0 MyAccessPoint 12345678")
 
 @fab.task
 def setup_wifi_sta():
@@ -76,7 +90,7 @@ def v_inf(type="tx"):
         fab.sudo = ("sudo ifconfig eth10 hw ether 00:22:22:ff:ff:f0")
         fab.sudo = ("sudo ip addr add 192.168.10.2/24 brd + dev eth10 label eth10:0")
 
-
+#@roles('vlc1')
 @fab.task
 def wifi_link():
     #fab.sudo = ("route add -net 192.168.10.0 gw 192.168.12.1 netmask 255.255.255.0 dev ap0")
@@ -85,6 +99,7 @@ def wifi_link():
     with cd('/home/debian/OpenVLC/Latest_Version/'):
         fab.sudo("./wifi_link.sh")
 
+#@roles('vlc1')
 @fab.task
 def vlc_link():
     #fab.sudo = ("route add -net 192.168.10.0 gw 192.168.0.1 netmask 255.255.255.0 dev vlc0")
@@ -98,17 +113,62 @@ def vlc_link():
 def schedule_controller():
     while True: 
         execute('vlc_link')
-        time.sleep(20)
+        time.sleep(5)
         execute('wifi_link')
-        time.sleep(20)
+        time.sleep(5)
 
+#Intelligent Control
 @fab.task
-def icontrol():
-    if type=="vlc":
-        command = "iperf -u -l 800 -s -i3 -B 192.168.0.2 -p 10001"
-    elif type=="wifi":
-        command = "iperf -u -l 800 -s -i3 -B 192.168.12.26 -p 10002"
-    else:
-        command = "iperf -u -l 800 -s -i3 -B 192.168.10.2 -p 10003"
-    fab.run(command, pty=False)
+def icontrol(capture=True):
+    T=1
+    Twatchdog=0.1
+    execute(vlc1)
+    execute(vlc_link)
+    current_state="VLC"
+    time.sleep(T)
 
+    while 1:
+        #Watchdog for VLC
+        if current_state=="WIFI":
+            execute(vlc1)
+            execute(vlc_link)
+            current_state="VLC"
+            time.sleep(Twatchdog)
+        #RX
+        execute(vlc2)
+        output = execute(getRSSI)
+        output=output[list(env.hosts)[0]].split(" ")
+        print(output)
+	output = [int(i) for i in output]
+        print("Checking the RSSI value in VLC channel: "+str(output))
+        #print("CURRENT STATE={}".format(current_state))
+
+        execute(vlc1)
+        
+        if output[0]<1061 and current_state=="VLC":
+                print("Switching to WiFi channel \n")
+                execute(wifi_link)
+                current_state="WIFI"
+        else:
+            #if current_state=="WIFI":
+                print("Switching to VLC channel \n")
+                execute(vlc_link)
+                current_state="VLC"
+	
+
+
+        """
+        if  output[0]>1060:
+            print("VLC channel is stable")
+        else:
+        #TX
+            execute(vlc1)
+            if output[0]<1061:
+                print("Switching to WiFi channel \n")
+                execute(wifi_link)
+            else:
+                print("Switching to VLC channel \n")
+                execute(vlc_link)
+        
+        """
+        time.sleep(T)
